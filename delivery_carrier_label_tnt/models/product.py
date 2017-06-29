4e24612b632c69db6cfc6c27d580e2b1b5f1ce88
James Curtis <james.curtis@opusvl.com>
2017-06-29 14:28:46 +0100

    [REM] Remove unnecessary dry ice field

--- delivery_carrier_label_tnt/models/product.py
+++ delivery_carrier_label_tnt/models/product.py
@@ -3,13 +3,10 @@
 from openerp import models, fields
 
 
+# Add hazardous product to product template, related to hazardous.codes model
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    dry_ice = fields.Boolean(
-        string='Dry Ice',
-        help='Does this product need to be packaged and shipped in dry ice'
-    )
     hazardous_id = fields.Many2one(
         comodel_name='hazardous.codes',
         string='Description',
