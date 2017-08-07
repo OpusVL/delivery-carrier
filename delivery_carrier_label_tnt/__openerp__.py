2e9c2ef64bfc3bf3c12634f531fc4dbfa762d66e
James Curtis <james.curtis@opusvl.com>
2017-08-07 17:06:24 +0100

    [REF] Update credits

--- delivery_carrier_label_tnt/__openerp__.py
+++ delivery_carrier_label_tnt/__openerp__.py
@@ -1,12 +1,30 @@
-# coding: utf-8
-# © 2015 David BEAL @ Akretion
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# -*- coding: utf-8 -*-
+
+# #########################################################################
+#
+# Delivery Carrier Label TNT
+# Copyright (C) 2017 OpusVL (<http://opusvl.com/>)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# ########################################################################
 
 {
     'name': 'Delivery Carrier Label TNT',
     'version': '8.0.0.1.0',
-    'author': "Akretion,Odoo Community Association (OCA)",
-    'maintener': 'Akretion',
+    'author': "OpusVL,Odoo Community Association (OCA)",
+    'maintainer': 'OpusVL',
     'category': 'Warehouse',
     'summary': "TNT carrier label printing",
     'depends': [
@@ -17,7 +35,7 @@
         'stock_packaging_usability',  # Might not be needed
         'document',
     ],
-    'website': 'http://www.akretion.com/',
+    'website': 'http://opusvl.com/',
     'data': [
         'data/delivery_carrier.xml',
         'data/sequence.xml',
